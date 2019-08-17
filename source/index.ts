6cfc6792a9091d742b15a77dacbc2f9f4149a207
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-08-16 20:25:57 -0400

    Finish ESLint configuration

---
+++ source/index.ts
@@ -0,0 +1,16 @@
+function hello (): string {
+    return "yorp";
+}
+
+const wut = () => {
+    return "hey"
+};
+
+const thingy = {
+    cosas: "hey",
+    ycosas: "you",
+};
+
+const cosas: string[] = [];
+
+
